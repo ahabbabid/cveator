bbec906b0206744d8fa70c0e4ce95d4d791cc0cf
u2018225 <u2018225@giki.edu.pk>
2022-01-31 23:28:56 +0500

    changes

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -59,7 +59,7 @@ const SignUpPage: NextPage<{
                 body: JSON.stringify(values),
               })
               if (res.status === 200) {
-                router.push('/resume')
+                router.replace('/resume')
               } else if (res.status === 401) {
                 const body = await res.json()
                 if (body.error) {
