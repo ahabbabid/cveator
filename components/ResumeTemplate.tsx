da292778d54542d4cf684ed811f2ae741e496cad
u2018225 <u2018225@giki.edu.pk>
2022-01-31 20:17:02 +0500

    fixed crossorigin

--- components/ResumeTemplate.tsx
+++ components/ResumeTemplate.tsx
@@ -6,7 +6,11 @@ const getHTMlFromResumeData = (resumeData: FormValues): ReactElement => {
     <div className="p-5">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
