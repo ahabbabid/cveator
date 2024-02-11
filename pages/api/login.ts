c778298d0be5006061e7453c08fd269e4a2af7ee
Muhammad Ahabb <ahabbabid@gmail.com>
2024-02-11 12:34:36 +0500

    logging for vercel

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -6,27 +6,31 @@ export default withSessionRoute(loginRoute)
 
 async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
   // get user from database then:
-  const reqBody = JSON.parse(req.body)
- console.log(process.env.TURSO_DATABASE_URL) 
-  const user = await prisma.users.findFirst({
-    where: {
-      username: reqBody.regNo,
-      password: reqBody.password,
-    },
-  })
-  if (!user)
-    res.status(400).send({
-      error: 'Regstration or Password Incorrect',
+  try {
+    const reqBody = JSON.parse(req.body)
+
+    const user = await prisma.users.findFirst({
+      where: {
+        username: reqBody.regNo,
+        password: reqBody.password,
+      },
     })
-  else {
-    req.session.user = user
+    if (!user)
+      res.status(400).send({
+        error: 'Regstration or Password Incorrect',
+      })
+    else {
+      req.session.user = user
 
-    await req.session.save()
-    // res.status(200).send({
-    //   ok: true,
-    // })
+      await req.session.save()
+      // res.status(200).send({
+      //   ok: true,
+      // })
 
-    res.setHeader('cache-control', 'no-store, max-age=0')
-    res.redirect('/resume')
+      res.setHeader('cache-control', 'no-store, max-age=0')
+      res.redirect('/resume')
+    }
+  } catch (e) {
+    console.log(e)
   }
 }
