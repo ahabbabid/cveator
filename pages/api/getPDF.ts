666b85a4ff57ca3ee3b4636dcf207dadeb6b8455
Muhammad Ahabb <ahabbabid@gmail.com>
2024-02-11 13:00:20 +0500

    fix bad head import in getPDF.ts

--- pages/api/getPDF.ts
+++ pages/api/getPDF.ts
@@ -1,4 +1,3 @@
-import { Head } from 'next/head';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { withSessionRoute } from '../../lib/withSession'
